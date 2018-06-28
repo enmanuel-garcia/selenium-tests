d36d552a82d764e3bc36fab6440ace173ed7ee11
DESKTOP-MR9Q9JP\melvin <guntanis666@hotmail.com>
2018-06-28 14:00:15 -0600

    Agregue test case 2

--- src/main/java/Selenium2Example.java
+++ src/main/java/Selenium2Example.java
@@ -3,7 +3,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,7 +12,7 @@ public class Selenium2Example  {
         // Create a new instance of the Firefox driver
         // Notice that the remainder of the code relies on the interface,
         // not the implementation.
-        WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new ChromeDriver();
 
         // And now use this to visit Google
         driver.get("http://www.google.com");
@@ -33,7 +33,7 @@ public class Selenium2Example  {
 
         // Google's search is rendered dynamically with JavaScript.
         // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+        (new WebDriverWait(driver, 3)).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver d) {
                 return d.getTitle().toLowerCase().startsWith("cheese!");
             }
