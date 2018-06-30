51961655344209ffef4f3d3d6928ddee287e269c
Enmanuel García <egarciag@ucenfotec.ac.cr>
2018-06-29 19:17:15 -0600

    Se añade el caso de prueba 1.

--- src/test/java/SeleniumTestCaseBase.java
+++ src/test/java/SeleniumTestCaseBase.java
@@ -1,9 +1,10 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumTestCaseBase {
+public abstract class SeleniumTestCaseBase {
     private final static String NAME_SYSTEM_PROPERTY	= "webdriver.chrome.driver";
     private final static String VALUE_SYSTEM_PROPERTY	= "../selenium-tests/src/test/java/chromedriver.exe";
     public final static String NOP_COMMERCE_URL       	= "http://demo.nopcommerce.com/";
@@ -15,6 +16,9 @@ public class SeleniumTestCaseBase {
     	System.setProperty(NAME_SYSTEM_PROPERTY, VALUE_SYSTEM_PROPERTY);
         this.driver = new ChromeDriver();
     }
+    
+    @Test
+    public abstract void TestCase();
     
     @AfterEach
     public void shutDownDriver() {
